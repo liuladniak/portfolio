ebbea87aa07a9bf60d8b3d6b3f7539f0fbc10ce1
Liu Ladniak <liuladniak@gmail.com>
2024-11-06 22:10:50 -0500

    cleaned up code from comments

--- src/pages/ConnectPage/ConnectPage.jsx
+++ src/pages/ConnectPage/ConnectPage.jsx
@@ -8,29 +8,6 @@ const ConnectPage = () => {
     <section className="connect">
       <HeroConnect />
       <ConnectLinks />
-      {/* <div className="connect-links">
-        <a
-          target="_blank"
-          href="https://www.linkedin.com/in/liuladniak/"
-          className="btn btn--connect"
-        >
-          <img
-            className="btn__icon"
-            src={linkedIcon}
-            alt="linkedin logo icon"
-          />
-          <span>LinkedIn</span>
-        </a>
-
-        <a
-          target="_blank"
-          href="mailto:liuladniak@gmail.com"
-          className="btn btn--connect"
-        >
-          <img className="btn__icon" src={emailIcon} alt="Mail icon" />
-          <span>Email</span>
-        </a>
-      </div> */}
     </section>
   );
 };
